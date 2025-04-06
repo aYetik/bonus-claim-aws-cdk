437a5cd86000393eff307d87a41c43c1c9f661f5
Anıl Yetik <anil.yetik@gmail.com>
2025-04-06 12:42:03 +0300

    add https certificates

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -8,17 +8,37 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager';
 
 interface EcsStackProps extends cdk.StackProps {
   vpc: ec2.IVpc;
   table: dynamodb.ITable;
   region: string;
-  envName: string;
+  envName: string; // 'dev' or 'prod'
 }
 
 export class EcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: EcsStackProps) {
     super(scope, id, props);
+
+    const hostedZone = route53.HostedZone.fromLookup(this, 'YetikHostedZone', {
+      domainName: 'yetik.net',
+    });
+
+    const suffix = props.envName === 'dev' ? '-dev' : '';
+    const userDomain = `user${suffix}.yetik.net`;
+    const adminDomain = `admin${suffix}.yetik.net`;
+
+    const userCert = new certificatemanager.Certificate(this, 'UserCert', {
+      domainName: userDomain,
+      validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
+    });
+    
+    const adminCert = new certificatemanager.Certificate(this, 'AdminCert', {
+      domainName: adminDomain,
+      validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
+    });
+
 
     const cluster = new ecs.Cluster(this, 'EveryrealmCluster', {
       vpc: props.vpc,
@@ -28,10 +48,10 @@ export class EcsStack extends cdk.Stack {
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
     });
 
-    props.table.grantReadWriteData(taskRole); //least privilege principle
+    props.table.grantReadWriteData(taskRole);
 
-    // user-service
-    const userServiceRepo = ecr.Repository.fromRepositoryName(this, 'UserRepo', 'user-service');
+    //USER SERVICE
+    const userRepo = ecr.Repository.fromRepositoryName(this, 'UserRepo', 'user-service');
     const userService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'UserService', {
       cluster,
       cpu: 256,
@@ -39,7 +59,7 @@ export class EcsStack extends cdk.Stack {
       desiredCount: 1,
       taskSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
       taskImageOptions: {
-        image: ecs.ContainerImage.fromEcrRepository(userServiceRepo, 'latest'),
+        image: ecs.ContainerImage.fromEcrRepository(userRepo, 'latest'),
         containerPort: 3000,
         environment: {
           TABLE_NAME: props.table.tableName,
@@ -53,6 +73,10 @@ export class EcsStack extends cdk.Stack {
           ],
         }),
       },
+      domainName: userDomain,
+      domainZone: hostedZone,
+      certificate: userCert,
+      redirectHTTP: true,
       publicLoadBalancer: true,
     });
 
@@ -64,8 +88,14 @@ export class EcsStack extends cdk.Stack {
       unhealthyThresholdCount: 2,
     });
 
-    // admin-service
-    const adminServiceRepo = ecr.Repository.fromRepositoryName(this, 'AdminRepo', 'admin-service');
+    new route53.ARecord(this, 'UserServiceAliasRecord', {
+      zone: hostedZone,
+      recordName: `user${suffix}`,
+      target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(userService.loadBalancer)),
+    });
+
+    //ADMIN SERVICE
+    const adminRepo = ecr.Repository.fromRepositoryName(this, 'AdminRepo', 'admin-service');
     const adminService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'AdminService', {
       cluster,
       cpu: 256,
@@ -73,7 +103,7 @@ export class EcsStack extends cdk.Stack {
       desiredCount: 1,
       taskSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
       taskImageOptions: {
-        image: ecs.ContainerImage.fromEcrRepository(adminServiceRepo, 'latest'),
+        image: ecs.ContainerImage.fromEcrRepository(adminRepo, 'latest'),
         containerPort: 3000,
         environment: {
           TABLE_NAME: props.table.tableName,
@@ -87,28 +117,19 @@ export class EcsStack extends cdk.Stack {
           ],
         }),
       },
+      domainName: adminDomain,
+      domainZone: hostedZone,
+      certificate: adminCert,
+      redirectHTTP: true,
       publicLoadBalancer: true,
     });
-    
+
     adminService.targetGroup.configureHealthCheck({
       path: '/',
       interval: cdk.Duration.seconds(30),
       timeout: cdk.Duration.seconds(5),
       healthyThresholdCount: 2,
       unhealthyThresholdCount: 2,
-    });
-
-    // Hosted zone
-    const hostedZone = route53.HostedZone.fromLookup(this, 'YetikHostedZone', {
-      domainName: 'yetik.net',
-    });
-
-    const suffix = props.envName === 'dev' ? '-dev' : '';
-
-    new route53.ARecord(this, 'UserServiceAliasRecord', {
-      zone: hostedZone,
-      recordName: `user${suffix}`,
-      target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(userService.loadBalancer)),
     });
 
     new route53.ARecord(this, 'AdminServiceAliasRecord', {
